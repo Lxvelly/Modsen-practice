62aeae7b43a38793ab1997fd87d28a59a10c14d6
lxvelly <aptem.otpaba@gmail.com>
2024-06-21 11:14:46 +0300

    feat: add books cards

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -7,14 +7,17 @@ import styles from './styles.module.scss'
 export const Body = () => {
   const [data, setData] = useState<any[]>([])
   const [loaded, setLoaded] = useState<boolean>(false)
+  const [totalItems, setTotalItems] = useState(0)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const resp = await axios.get(
-          `https://www.googleapis.com/books/v1/volumes?q=:keyes&key=${key}`,
+          `https://www.googleapis.com/books/v1/volumes?q=javascript&maxResults=30&key=${key}`,
         )
         const books = resp.data.items || []
+        const totalItemsCount = resp.data.totalItems
+        setTotalItems(totalItemsCount)
         setData(books)
         setLoaded(true)
       } catch (error) {
@@ -27,13 +30,29 @@ export const Body = () => {
   return (
     <div className={styles.Body}>
       {loaded ? (
-        <div className={styles.Body__books}>
-          {data.map((e) => (
-            <div key={e.id} className={styles.Body__books__item}>
-              {e.volumeInfo.title}
-            </div>
-          ))}{' '}
-        </div>
+        <>
+          <p className={styles.Body__text}>Found {totalItems} results</p>
+          <div className={styles.Body__books}>
+            {data.map((e) => (
+              <div key={e.id} className={styles.Body__books__item}>
+                <img
+                  src={e.volumeInfo.imageLinks.thumbnail}
+                  alt="Thumbnail"
+                  className={styles.Body__books__item__img}
+                />
+                <div className={styles.Body__books__item__link}>
+                  <a href="/">{e.volumeInfo.categories}</a>
+                </div>
+                <div className={styles.Body__books__item__name}>
+                  {e.volumeInfo.title}
+                </div>
+                <div className={styles.Body__books__item__authors}>
+                  {e.volumeInfo.authors.join(', ')}
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
       ) : (
         <p>Loading...</p>
       )}
